c779a4f6010da53117e8cd9464043d9647ed6612
Jose Luiz Rodrigues <zeluizr@me.com>
2022-10-28 14:14:29 -0300

    monster

--- src/npc/monsters.js
+++ src/npc/monsters.js
@@ -2,11 +2,5 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async function getMonster(id) {
-  const monster = await prisma.monsters.findOne({
-    where: {
-      id,
-    },
-  });
-
-  return monster;
+  return null;
 };
